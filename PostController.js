8bc733711368008e7329b5936c7b3cdf072464fb
Alexsandr <alexnem@inbox.ru>
2021-04-17 15:56:09 +0300

    added CRUD controllers

--- PostController.js
+++ PostController.js
@@ -1,7 +1,7 @@
 import Post from './Post.js';
 
 class PostController {
-  async create (req, res) {
+  async create(req, res) {
     try {
       const {author, title, content, picture} = req.body;
       const post = await Post.create({author, title, content, picture});
@@ -12,16 +12,48 @@ class PostController {
   }
 
   async getAll(req, res) {
-
+    try {
+      const posts = await Post.find();
+      return res.json(posts);
+    } catch(e) {
+      res.status(500).json(e);
+    }
   }
   async getOne(req, res) {
-
+    try {
+      const {id} = req.params;
+      if (!id) {
+        return res.status(400).json({message: 'Id не указан'});
+      }
+      const post = await Post.findById(id);
+      return res.json(post);
+    } catch(e) {
+      res.status(500).json(e);
+    }
   }
   async update(req, res) {
-
+    try {
+      const post = req.body;
+      if (!post._id) {
+        return res.status(400).json({message: 'Id не указан'});
+      }
+      const updatedPost = await Post.findByIdAndUpdate(post._id, post, {new: true});
+      return res.json(updatedPost);
+    } catch(e) {
+      res.status(500).json(e);
+    }
   }
   async delete(req, res) {
-
+    try {
+      const {id} = req.params;
+      if (!id) {
+        return res.status(400).json({message: 'Id не указан'});
+      }
+      const post = await Post.findByIdAndDelete(id);
+      return res.json(post);
+    } catch(e) {
+      res.status(500).json(e);
+    }
   }
 }
 
